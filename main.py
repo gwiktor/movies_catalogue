9b5583f6aa6cb0321b084e5b5c61b1a9a5d02e9a
Wiktor Gabrys <gabrys.wiktor@gmail.com>
2020-07-11 23:27:24 +0200

    Upgrade code

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
+import tmdb_client
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    movies = [i for i in range(20)]
-    return render_template("homepage.html", movies=movies)
+    selected_list = request.args.get('list_type', "popular")
+    movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
+    return render_template("homepage.html", movies=movies, current_list=selected_list)
+
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb_client.get_poster_url(path, size)
+    def tmdb_title_url(path):
+        return tmdb_client.get_movie_info(path)
+    def tmdb_imageb_url(path, size):
+        return tmdb_client.get_image_url(path, size)
+    return {"tmdb_title_url": tmdb_title_url, "tmdb_image_url": tmdb_image_url, "tmdb_imageb_url": tmdb_imageb_url}
+
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+   details = tmdb_client.get_single_movie(movie_id)
+   cast = tmdb_client.get_single_movie_cast(movie_id, how_many=4)
+   return render_template("movie_details.html", movie=details, cast=cast)
 
 if __name__ == '__main__':
     app.run(debug=True)
