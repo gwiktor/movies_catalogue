08ad5a9b0c7c28f7ed165ddc36c6177f862899d1
Wiktor Gabrys <gabrys.wiktor@gmail.com>
2020-07-20 12:36:19 +0200

    Modify code main.py

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
+    movie_lists = ['now_playing', 'popular', 'top_rated', 'upcoming']
     selected_list = request.args.get('list_type', "popular")
+    if selected_list not in movie_lists:
+        selected_list = 'popular'
     movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
-    return render_template("homepage.html", movies=movies, current_list=selected_list)
+    return render_template("homepage.html", movies=movies, current_list=selected_list, movie_lists=movie_lists)
 
 @app.context_processor
 def utility_processor():
@@ -24,6 +27,8 @@ def movie_details(movie_id):
    details = tmdb_client.get_single_movie(movie_id)
    cast = tmdb_client.get_single_movie_cast(movie_id, how_many=4)
    return render_template("movie_details.html", movie=details, cast=cast)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
